942bd0a0e7630de243d9a2d39acfd02829505488
Taros Nyu <niutartaros@gmail.com>
2015-01-24 01:19:25 +0800

    Update SAMPLE_C_0ms_168kB.c

--- SAMPLE_C_0ms_168kB.c
+++ SAMPLE_C_0ms_168kB.c
@@ -7,25 +7,25 @@
 
 int main()
 {
-    char src[MaxN];     //octal decimals
+    char src[MaxN];     //八进制小数
     int i,j;
     while(scanf("%s", src) != EOF)
     {
-        char dest[MaxN]={'0'};      //decimal
-        int index=0;                //length of the decimal
+        char dest[MaxN]={'0'};      //十进制小数
+        int index=0;                //十进制小数的长度
         for(i=strlen(src)-1; i>1; i--)
         {
-            int num= src[i] - '0';      //current place of the octal decimals
+            int num= src[i] - '0';      //八进制小数处理的当前位
             int temp;
             for(j=0; j<index || num; j++)
             {
                 temp = num*10 + (j<index?dest[j]-'0':0);
-                dest[j] = temp/8+'0';       //quetient
-                num = temp%8;               //remainder
+                dest[j] = temp/8+'0';       //商
+                num = temp%8;               //余数
             }
-            index = j;                      //actual digital of the decimal
+            index = j;                      //十进制小数的实际位数
         }
-        dest[j]='\0';                       //end of string
+        dest[j]='\0';                       //字符串结束标志
         printf("%s [8] = 0.%s [10]\n", src,dest);
     }
     return 0;
